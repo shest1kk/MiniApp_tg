2675d35ebb54fc024164fbfe3493913eed354cb8
shest1kk <tux703404@gmail.com>
2024-06-21 22:44:32 +0300

    123

--- src/components/Pages/FAQ/FAQ.jsx
+++ src/components/Pages/FAQ/FAQ.jsx
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './FAQ.css';
 import Button from '../../Buttons/Button';
 import { Link } from "react-router-dom";
+
 const FAQ = () => {
+    useEffect(() => {
+        const img = new Image();
+        img.src = require('../../../assets/1.jpg');  // Adjust the path as per your actual image location
+    }, []);
+
     return (
         <div className={'FAQ_wrapper'}>
-        <div className={'FAQ_image'}>
-        <img className={'Faq_image_img'}
-            src={require('../../../assets/1.jpg')}
-            alt="123"
+            <div className={'FAQ_image'}>
+                <img className={'Faq_image_img'}
+                     src={require('../../../assets/1.jpg')}  // Ensure this path matches the one in useEffect
+                     alt="123"
                 />
-        </div>
-        <div className={'FAQ_text'}>
-            <span>Общая информация</span>
             </div>
-        <div className={'FAQ_categories'}>
-            <Link to='about'>
-            <Button className={'primary-button'}>Что входит</Button>
-            </Link>
-            <Link to='map'>
-            <Button className={'primary-button'}>Карта</Button>
-            </Link>
-            <Link to='living'>
-            <Button className={'primary-button'}>Проживание</Button>
-            </Link>
-            <Link to='/sections'>
-            <Button className={'primary-button'}>Назад</Button>
-            </Link>
-        
-        </div>
+            <div className={'FAQ_text'}>
+                <span>Общая информация</span>
+            </div>
+            <div className={'FAQ_categories'}>
+                <Link to='about'>
+                    <Button className={'primary-button'}>Что входит</Button>
+                </Link>
+                <Link to='map'>
+                    <Button className={'primary-button'}>Карта</Button>
+                </Link>
+                <Link to='living'>
+                    <Button className={'primary-button'}>Проживание</Button>
+                </Link>
+                <Link to='/sections'>
+                    <Button className={'primary-button'}>Назад</Button>
+                </Link>
+            </div>
         </div> 
     );
 };
--- src/components/Pages/FAQ/FaqAbout/FaqAbout.jsx
+++ src/components/Pages/FAQ/FaqAbout/FaqAbout.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import './FaqAbout.css';
-import Button from '../../../Buttons/Button';
-import { Link } from "react-router-dom";
+    import React from "react";
+    import './FaqAbout.css';
+    import Button from '../../../Buttons/Button';
+    import { Link } from "react-router-dom";
 
 
-const FaqAbout = () => {
-    return (
-        <div className={'FaqAbout_wrapper'}>
-        <div className={'FaqAbout_image'}>
-        <img className={'FaqAbout_image_img'}
-            src={require('../../../../assets/background-main.jpg')}
-            alt="123"
-                />
-        </div>
-        <div className={'FaqAbout_text'}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Dignissimos cumque dolorum totam a enim libero? Deserunt dolorum modi, eius quos atque facilis repellendus. 
-            Accusantium libero perspiciatis voluptate fugiat soluta sint tempora eos.
-        </div>
-            <div className={'FaqAbout-button'}>
-            <Link to='/faq'>
-            <Button className={'primary-button'}>Назад</Button>
-            </Link>
-            
-        </div>
-        </div>
-    );
-};
+    const FaqAbout = () => {
+        return (
+            <div className={'FaqAbout_wrapper'}>
+            <div className={'FaqAbout_image'}>
+            <img className={'FaqAbout_image_img'}
+                src={require('../../../../assets/background-main.jpg')}
+                alt="123"
+                    />
+            </div>
+            <div className={'FaqAbout_text'}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Dignissimos cumque dolorum totam a enim libero? Deserunt dolorum modi, eius quos atque facilis repellendus. 
+                Accusantium libero perspiciatis voluptate fugiat soluta sint tempora eos.
+            </div>
+                <div className={'FaqAbout-button'}>
+                <Link to='/faq'>
+                <Button className={'primary-button'}>Назад</Button>
+                </Link>
+                
+            </div>
+            </div>
+        );
+    };
 
-export default FaqAbout;
+    export default FaqAbout;
