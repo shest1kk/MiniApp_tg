27c93a21bb77101a2dfa482caf0265efe9acdc00
Пахарев Егор <e.pakharev@akrussia.com>
2024-06-21 09:42:00 +0300

    1234

--- src/components/Pages/FAQ/FaqAbout/FaqAbout.jsx
+++ src/components/Pages/FAQ/FaqAbout/FaqAbout.jsx
@@ -6,11 +6,17 @@ const FaqAbout = () => {
     return (
         <div className={'FaqAbout_wrapper'}>
         <div className={'FaqAbout_image'}>
+        <img className={'FaqAbout_image_img'}
+            src={require('../../../../assets/background-main.jpg')}
+            alt="123"
+                />
         </div>
-        <div className={'FaqAbout_text'}>
+        <div className={'FaqAbout_text'}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Dignissimos cumque dolorum totam a enim libero? Deserunt dolorum modi, eius quos atque facilis repellendus. 
+            Accusantium libero perspiciatis voluptate fugiat soluta sint tempora eos.
         </div>
             <div className={'FaqAbout-button'}>
-            <Link to='/FAQ'>
+            <Link to='/faq'>
             <Button className={'primary-button'}>Назад</Button>
             </Link>
             
