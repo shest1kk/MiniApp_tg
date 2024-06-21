dc7064d5c34c9769639977aa3e69ef21509e8cb4
Пахарев Егор <e.pakharev@akrussia.com>
2024-06-21 11:11:37 +0300

    1234

--- src/components/Pages/Feedback/Feedback.jsx
+++ src/components/Pages/Feedback/Feedback.jsx
@@ -3,24 +3,17 @@ import './Feedback.css';
 import Button from '../../Buttons/Button';
 import { Link } from "react-router-dom";
 
-// Функция для копирования текста и вывода alert
+// Функция для копирования текста и вывода alert через Telegram WebApp
 const handleCopyText = () => {
-  const textToCopy = '+79969184707';
+  const textToCopy = '+7 (996) 918-47-07';
 
-  // Создаем временный элемент для копирования текста
-  const textarea = document.createElement('textarea');
-  textarea.value = textToCopy;
-  document.body.appendChild(textarea);
-
-  // Выделяем текст и копируем его
-  textarea.select();
-  document.execCommand('copy');
-
-  // Удаляем временный элемент
-  document.body.removeChild(textarea);
-
-  // Выводим alert
-  alert('Вы скопировали номер телефона');
+  // Копируем текст в буфер обмена
+  navigator.clipboard.writeText(textToCopy).then(() => {
+    // Выводим alert через Telegram WebApp
+    window.Telegram.WebApp.showAlert('Вы скопировали номер телефона');
+  }).catch(err => {
+    console.error('Failed to copy text: ', err);
+  });
 };
 
 const Feedback = () => {
@@ -29,14 +22,13 @@ const Feedback = () => {
       <div className={'Feedback_text'}>
         <span>Обратная связь</span>
       </div>
-      <div className={'Feedback_categories'}>
-        <a target='_blank' href="https://t.me/feyhoa779">
+      <div className={'Meetup_categories'}>
+      <a target='_blank' href="https://t.me/feyhoa779">
           <Button className={'primary-button'}>Телеграм</Button>
         </a>
         <a href="https://wa.me/79969184707">
           <Button className={'primary-button'}>WhatsApp</Button>
         </a>
-        
         <Button className={'primary-button'} onClick={handleCopyText}>Телефон</Button>
         <Link to='/sections'>
           <Button className={'primary-button'}>Назад</Button>
