f83491b0971136181ad56c1c4b295f12db4d0162
Пахарев Егор <e.pakharev@akrussia.com>
2024-06-27 15:05:29 +0300

    test logo

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -52,14 +52,14 @@ const AppRoutes = () => {
       opacity: 1,
       scale: 1,
       transition: {
-        duration: 1,
+        duration: 0.66,
       },
     },
     exit: {
       opacity: 0,
       scale: 0.8,
       transition: {
-        duration: 1,
+        duration: 0.66,
       },
     },
   };
