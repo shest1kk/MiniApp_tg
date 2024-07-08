003e391f1515e16aa2dc93333e6485d626451062
Пахарев Егор <e.pakharev@akrussia.com>
2024-07-08 15:57:53 +0300

    123

--- src/components/Pages/FAQ/FaqAbout/FaqAbout.jsx
+++ src/components/Pages/FAQ/FaqAbout/FaqAbout.jsx
@@ -18,15 +18,36 @@ const FaqAbout = () => {
                 />
             </div>
             <div className={'FaqAbout_text'}>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Dignissimos cumque dolorum totam a enim libero? Deserunt dolorum modi, eius quos atque facilis repellendus. 
-                Accusantium libero perspiciatis voluptate fugiat soluta sint tempora eos.
+                <div className="FaqAbout_text_wrapper">
+                <div className="FaqAbout_text_Eating">
+                🍽️ Питание:
+                <ul>
+                    <li>завтрак</li>
+                    <span className={'details'}>с 8:00 - 10:00</span>
+                    <li>обед</li>
+                    <span className={'details'}>с 13:00 - 15:00</span>
+                </ul>
+                </div>
+                <div className="FaqAbout_text_Gym">
+                🏋Тренажерный зал<br />
+                <span className={'details'}>круглосуточно</span>
+                </div>
+                <div className="FaqAbout_text_Games">
+                💪Спортивные игры:
+                <ul>
+                    <li>Бильярд - американский пул с профессиональным маркером</li>
+                    <span className={'details'}>круглосуточно</span>
+                    <li>Настольный теннис</li>
+                </ul>
             </div>
             <div className={'FaqAbout-button'}>
                 <Link to='/faq'>
                     <Button className={'primary-button'}>Назад</Button>
                 </Link>
             </div>
+                </div>
+            </div>
+            
         </div>
     );
 };
